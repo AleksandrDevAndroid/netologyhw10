76e8bc63f4b72f9ac650deb611bf61f558643cff
AleksandrDevAndroid <aleksperdev@gmail.com>
2025-04-21 01:23:40 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        chekingPassword();
+        PasswordChecker checker = new PasswordChecker();
+        checker.setSetMinLength(5);
+        checker.verify("pass");
+    }
+
+    static public void chekingPassword() {
+        PasswordChecker passwordChecker = new PasswordChecker();
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        while (flag) {
+
+            System.out.print("Введите мин. длину пароля:\n");
+            int inputLength = Integer.parseInt(sc.nextLine());
+            passwordChecker.setSetMinLength(inputLength);
+            System.out.print("Введите макс. допустимое количество повторений символа подряд:\n");
+            int inputMaxRepeat = Integer.parseInt(sc.nextLine());
+            passwordChecker.setSetMaxRepeats(inputMaxRepeat);
+            System.out.println("Введите пароль или end");
+            String inputPassword = sc.nextLine();
+
+            if (inputPassword.equalsIgnoreCase("end")) {
+                break;
+            }
+            if (passwordChecker.verify(inputPassword) == false) {
+                System.out.println("Не подходит!");
+            } else System.out.println("Подходит!");
+
+        }
+    }
+}
